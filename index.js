e26c706d92fa1a08be5ac66f0fe6439ba763721c
jahid <jahidhasanbsl46@gmail.com>
2026-05-21 18:59:51 +0600

    update port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { jwtVerify, createRemoteJWKSet } = require("jose-cjs");
 const uri = process.env.MONGO_URI;
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
